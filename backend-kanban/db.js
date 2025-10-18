ef7fe5c5d3d145f52e280d5723840475e4a80b4a
Elisamuel Valera Socarrás <98656922+Elisamuel04@users.noreply.github.com>
2025-10-18 10:19:15 -0500

    cargando archivos  .env de dotenv

--- backend-kanban/db.js
+++ backend-kanban/db.js
@@ -1,12 +1,14 @@
 import pkg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'kanban_db',
-  password: 'samuel',
-  port: 5433,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: parseInt(process.env.PGPORT, 10),
 });
 
 export default pool;
