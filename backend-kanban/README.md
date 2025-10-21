5c8893dbefabd6ddfc33db43e2a247813cf5d569
Elisamuel Valera Socarrás <98656922+Elisamuel04@users.noreply.github.com>
2025-10-21 17:37:31 -0500

    database intructions

--- backend-kanban/README.md
+++ backend-kanban/README.md
@@ -1,2 +1,5 @@
 # backend
 Backend en Nodejs MiniKanban proyecto de Elisamuel Valera para la prueba Tec. Dev Full Stack (Node.js + Angular)
+
+# Ejecutar este comando en postgresql V18 o inferior para crear la base de datos y tablas
+psql -U postgres -d kanban_db -f database/init.sql
---
+++ backend-kanban/database/init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS users (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  username VARCHAR(100) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS tasks (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  status VARCHAR(50) DEFAULT 'todo',
+  createdAt TIMESTAMP DEFAULT NOW(),
+  user_id UUID REFERENCES users(id) ON DELETE CASCADE
+);
